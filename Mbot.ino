dcd2883e9417e895ef1ec7eabb2920b6089ac7d1
aldemar3002 <77545156+aldemar3002@users.noreply.github.com>
2024-06-13 18:10:23 -0600

    Update Mbot.ino

--- Mbot.ino
+++ Mbot.ino
@@ -10,9 +10,6 @@
 #define FORWARD 1
 #define BACKWARD 0
 
-int currentDirection = FORWARD; // Variable para mantener la dirección actual
-bool manualMode = false; // Variable para controlar el modo manual o automático
-
 #define USART_BAUDRATE 9600
 #define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
@@ -27,8 +24,8 @@ void setupPWM() {
     // Configurar Timer3 para Motor 3 y Motor 4
     TCCR3A = (1 << WGM30) | (1 << COM3A1) | (1 << COM3B1); // Modo PWM de 8 bits, Clear OC3A/OC3B on Compare Match
     TCCR3B = (1 << WGM32) | (1 << CS31); // Prescaler 8
-    OCR4B = 102; // 40% de 255 (8-bit PWM)
-    OCR4C = 102; // 40% de 255 (8-bit PWM)
+    OCR3A = 102; // 40% de 255 (8-bit PWM)
+    OCR3B = 102; // 40% de 255 (8-bit PWM)
 }
 
 // Inicialización de UART2
@@ -64,49 +61,49 @@ char receiveUART2(void) {
 
 // Funciones de movimiento para cada motor
 void runForwardM1() {
-    PORTB |= (1 << PB5);  // Encender Motor 1 (OC1A)
+    OCR1A = 102; // Ajustar el ciclo de trabajo de PWM para el motor 1
     PORTC |= (1 << PC4);  // Configurar dirección hacia adelante
     PORTC &= ~(1 << PC5); // Apagar la dirección opuesta
 }
 
 void runBackwardM1() {
-    PORTB |= (1 << PB5);  // Encender Motor 1 (OC1A)
+    OCR1A = 102; // Ajustar el ciclo de trabajo de PWM para el motor 1
     PORTC |= (1 << PC5);  // Configurar dirección hacia atrás
     PORTC &= ~(1 << PC4); // Apagar la dirección opuesta
 }
 
 void runForwardM2() {
-    PORTB |= (1 << PB6);  // Encender Motor 2 (OC1B)
+    OCR1B = 102; // Ajustar el ciclo de trabajo de PWM para el motor 2
     PORTC |= (1 << PC2);  // Configurar dirección hacia adelante
     PORTC &= ~(1 << PC3); // Apagar la dirección opuesta
 }
 
 void runBackwardM2() {
-    PORTB |= (1 << PB6);  // Encender Motor 2 (OC1B)
+    OCR1B = 102; // Ajustar el ciclo de trabajo de PWM para el motor 2
     PORTC |= (1 << PC3);  // Configurar dirección hacia atrás
     PORTC &= ~(1 << PC2); // Apagar la dirección opuesta
 }
 
 void runForwardM3() {
-    PORTH |= (1 << PH4);  // Encender Motor 3 (OC3A)
+    OCR3A = 102; // Ajustar el ciclo de trabajo de PWM para el motor 3
     PORTG |= (1 << PG0);  // Configurar dirección hacia adelante
     PORTG &= ~(1 << PG1); // Apagar la dirección opuesta
 }
 
 void runBackwardM3() {
-    PORTH |= (1 << PH4);  // Encender Motor 3 (OC3A)
+    OCR3A = 102; // Ajustar el ciclo de trabajo de PWM para el motor 3
     PORTG |= (1 << PG1);  // Configurar dirección hacia atrás
     PORTG &= ~(1 << PG0); // Apagar la dirección opuesta
 }
 
 void runForwardM4() {
-    PORTH |= (1 << PH5);  // Encender Motor 4 (OC3B)
+    OCR3B = 102; // Ajustar el ciclo de trabajo de PWM para el motor 4
     PORTC |= (1 << PC0);  // Configurar dirección hacia adelante
     PORTC &= ~(1 << PC1); // Apagar la dirección opuesta
 }
 
 void runBackwardM4() {
-    PORTH |= (1 << PH5);  // Encender Motor 4 (OC3B)
+    OCR3B = 102; // Ajustar el ciclo de trabajo de PWM para el motor 4
     PORTC |= (1 << PC1);  // Configurar dirección hacia atrás
     PORTC &= ~(1 << PC0); // Apagar la dirección opuesta
 }
@@ -157,29 +154,11 @@ void turnLeftTask(void *pvParameters) {
 }
 
 // Funciones de movimiento para todos los motores
-void runForward() {
-    TCCR1A |= (1<<COM1A1)| (1<<COM1B1);
-    TCCR4A |= (1<<COM4B1) | (1<<COM4C1);
-    runForwardM1();
-    runForwardM2();
-    runForwardM3();
-    runForwardM4();
-}
-
-void runBackward() {
-    TCCR1A |= (1<<COM1A1)| (1<<COM1B1);
-    TCCR4A |= (1<<COM4B1) | (1<<COM4C1);
-    runBackwardM1();
-    runBackwardM2();
-    runBackwardM3();
-    runBackwardM4();
-}
-
 void stopMotor() {
-    TCCR1A &= ~((1<<COM1A1) | (1<<COM1B1));
-    TCCR4A &= ~((1<<COM4B1) | (1<<COM4C1));
-    PORTB &= ~((1 << PB5) | (1 << PB6));
-    PORTH &= ~((1 << PH4) | (1 << PH5));
+    OCR1A = 0;
+    OCR1B = 0;
+    OCR3A = 0;
+    OCR3B = 0;
 }
 
 // Tareas de FreeRTOS
@@ -187,76 +166,28 @@ void manualControlTask(void *pvParameters) {
     while (1) {
         if (UCSR2A & (1 << RXC2)) { // Si hay datos disponibles en UART2
             char command = receiveUART2(); // Leer el comando
-            if (command == 'F' || command == 'X' || command == 'R' || command == 'L' || command == 'G' || command == 'M' || command == 'Q') {
+            if (command == 'F' || command == 'X' || command == 'R' || command == 'L' || command == 'G') {
                 switch (command) {
                     case 'F':
-                        if (manualMode) {
-                            xTaskCreate(runForwardTask, "RunForward", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-                        }
+                        xTaskCreate(runForwardTask, "RunForward", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
                         break;
                     case 'X':
-                        if (manualMode) {
-                            stopMotor();
-                            vTaskDelete(NULL); // Eliminar la tarea actual
-                        }
+                        stopMotor();
+                        vTaskDelete(NULL); // Eliminar la tarea actual
                         break;
                     case 'R':
-                        if (manualMode) {
-                            xTaskCreate(turnRightTask, "TurnRight", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-                        }
+                        xTaskCreate(turnRightTask, "TurnRight", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
                         break;
                     case 'L':
-                        if (manualMode) {
-                            xTaskCreate(turnLeftTask, "TurnLeft", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-                        }
+                        xTaskCreate(turnLeftTask, "TurnLeft", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
                         break;
                     case 'G':
-                        if (manualMode) {
-                            xTaskCreate(runBackwardTask, "RunBackward", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-                        }
-                        break;
-                    case 'M':
-                        if (!manualMode) {
-                            manualMode = true;
-                            stopMotor(); // Detener motores al cambiar a modo manual
-                        }
-                        break;
-                    case 'Q':
-                        if (manualMode) {
-                            manualMode = false;
-                            stopMotor(); // Detener motores al cambiar a modo automático
-                            currentDirection = FORWARD; // Asegurar que la dirección actual es hacia adelante
-                        }
+                        xTaskCreate(runBackwardTask, "RunBackward", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
                         break;
                 }
             }
         }
         vTaskDelay(pdMS_TO_TICKS(10)); // Retardo de 10 ms
-    }
-}
-
-void collisionTask(void *pvParameters) {
-    while (1) {
-        if (!manualMode) { // Solo ejecutar la lógica de colisión en modo automático
-            if (currentDirection == FORWARD) {
-                if (PINF & (1 << PF6)) { // Sensor frontal detecta un obstáculo
-                    runForward();
-                } else {
-                    currentDirection = BACKWARD;
-                    xTaskCreate(runBackwardTask, "RunBackward", configMINIMAL_STACK_SIZE, NULL, 1, NULL); // Cambiar a marcha atrás
-                    sendUART2('W'); // Envía el comando para atrasar canción
-                }
-            } else if (currentDirection == BACKWARD) {
-                if ((PINK & (1 << PK3))) { // Sensor trasero detecta un obstáculo
-                    runBackward();
-                } else {
-                    currentDirection = FORWARD;
-                    xTaskCreate(runForwardTask, "RunForward", configMINIMAL_STACK_SIZE, NULL, 1, NULL); // Cambiar a marcha adelante
-                    sendUART2('S'); // Envía el comando para adelantar canción
-                }
-            }
-        }
-        vTaskDelay(pdMS_TO_TICKS(100)); // Retardo de 100 ms
     }
 }
 
@@ -270,10 +201,6 @@ void setup() {
     PORTB &= ~((1 << PB5) | (1 << PB6));
     PORTH &= ~((1 << PH4) | (1 << PH5));
 
-    // Configuración de pines para los sensores
-    DDRF &= ~(1 << PF6); // PF6 como entrada (sensor frontal)
-    DDRK &= ~(1 << PK3); // PK3 como entrada (sensor trasero)
-
     // Inicialización de comunicaciones serie
     Serial.begin(9600); // Comunicación para depuración a 9600 baudios
     initUART2(); // Inicializar UART2
@@ -282,8 +209,6 @@ void setup() {
 
     // Crear tarea para controlar el Mbot en modo manual
     xTaskCreate(manualControlTask, "ManualControl", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    // Crear tarea para manejar colisiones en modo automático
-    xTaskCreate(collisionTask, "CollisionTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     // Habilitar interrupciones globales
     sei();
